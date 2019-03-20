f05cb699b1611edd083116f4f0a5c90067119481
ZhuofeiXieKatlin <zhx34@pitt.edu>
2019-03-20 16:09:26 -0400

    lab1

---
+++ lab1/exercise4.py
@@ -0,0 +1,10 @@
+print("-------guess---------")
+temp=input("the number in my mind is ")
+if guess == 8:
+    print("This is correct!")
+    print("you know me!")
+else:
+    print("wrong answer!")
+    print("gusses again!")
+print("endsing")
+
